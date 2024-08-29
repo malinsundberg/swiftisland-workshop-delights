e0d53c63182807bad80f2d9549aadaab40b5844e
Malin Sundberg <mail@malinsundberg.com>
2024-08-29 13:27:52 +0200

    Adds SFSymbol examples

--- SwiftIsland Delights/Exploration/ExplorationsView.swift
+++ SwiftIsland Delights/Exploration/ExplorationsView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct ExplorationsView: View {
+    @State private var isMuted: Bool = false
+    
     var body: some View {
-        Text("Hello, Swift Island!")
+        VStack(spacing: 14) {
+            Image(systemName: "party.popper")
+                .symbolEffect(.wiggle.counterClockwise, options: .repeat(.continuous).speed(0.5), isActive: true)
+            
+            Image(systemName: "fan.desk")
+                .symbolEffect(.rotate)
+            
+            Image(systemName: "heart.circle")
+                .symbolEffect(.breathe, options: .repeat(3))
+            
+            Button {
+                isMuted.toggle()
+            } label: {
+                Image(systemName: isMuted ? "bell.slash.fill" : "bell.fill")
+                    
+            }
+            .buttonStyle(.plain)
+            .contentTransition(.symbolEffect(.replace))
+        }
+        .foregroundStyle(Color.accentColor)
+        .bold()
+        .font(.largeTitle)
     }
 }
 
